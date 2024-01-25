3ef6b7a4d836cc813d89e3266b31f1315af0fda9
SamSam <samuel.lavazais@hotmail.fr>
2024-01-25 17:36:31 +0100

    sidepanel : deleteSession + openSession discards newtab

--- sidepanel/sidepanel.js
+++ sidepanel/sidepanel.js
@@ -1,5 +1,7 @@
 const sessionDisplay = document.querySelector("#sessionsList");
 
+const template = document.getElementById("sessionTemplate");
+
 document
     .querySelector("#sessionPrint")
     .addEventListener("click", printSessions);
@@ -9,8 +11,9 @@ async function printSessions() {
     await chrome.storage.local.get().then((storage) => {
         console.log("sessions stockées : ", storage);
         for (let session in storage) {
-            console.log(session);
-            sessionDisplay.innerHTML += `<div class="session">${session}</div>`;
+            let element = template.content.cloneNode(true);
+            element.querySelector(".sessionName").textContent = session;
+            sessionDisplay.appendChild(element);
         }
     });
 }
@@ -22,36 +25,63 @@ document
 
 async function handleNewSession() {
     // get newSession name
-    const newSessionTitle = document.getElementById("newSessionTitle").value;
+    const inputBox = document.getElementById("newSessionTitle")
+    const newSessionTitle = inputBox.value;
+    inputBox.value = ""
     // send message with this name to background => so it get current tabs & saves  it as a new session
-    await chrome.runtime.sendMessage({
-        type: "toSave",
-        newTitle: newSessionTitle,
-    });
+    if (newSessionTitle) {
+        await chrome.runtime.sendMessage({
+            type: "toSave",
+            newTitle: newSessionTitle,
+        });
 
-    // get currentSession newly stored in chrome.storage and print it
-    printSessions();
+        // get currentSession newly stored in chrome.storage and print it
+        printSessions();
+    }
+
 }
 
 sessionDisplay.addEventListener("click", function (event) {
-    openSession(event.target.textContent);
-
-    // if (event.target.parentNode.className === "delete") {
-    //     console.log("on va supprimer un truc");
-    //     deleteWord(event);
-    // } else if (event.target.parentNode.className === "saveTranslation") {
-    //     saveTranslation(event); // TODO
-    // } else if (event.target.parentNode.className === "launchSearch") {
-    //     lookupWord(event.target.dataset.word);
-    // }
+    console.log(event.target.className);
+    if (event.target.parentNode.className === "deleteSession") {
+        deleteSession(event);
+    } else if (event.target.className === "sessionName") {
+        openSession(event.target.textContent);
+    }
 });
 
 async function openSession(sessionTitle) {
     // obtenir le storage pour la session désirée
-    const tabsArray = await chrome.storage.local.get()
+    const tabsArray = await chrome.storage.local
+        .get()
         .then((storage) => storage[sessionTitle]);
     // obtenir la liste des urls à ouvrir
-        const urls = tabsArray.map((tab) => tab.URL);
-    // 
-    chrome.windows.create({ focused:false, url: urls})
+    const urls = tabsArray.map((tab) => tab.URL);
+
+    // ouvrir la fenêtre avec les onglets de la session
+    chrome.windows.create({ focused: true, state:"maximized", url: urls });
+    // CHECK : si un seul onglet est ouvert et qu'il s'agit d'un onglet newTab ou d'un onglet de recherche Google
+    await chrome.windows.getCurrent({populate:true}).then(window => {
+        if (window.tabs.length === 1 && (window.tabs[0].url === "chrome://extensions/" || window.tabs[0].url === "chrome://newtab/")) {
+            console.log("à supprimer");
+            chrome.windows.remove(window.id)
+        }
+    })
+    
 }
+
+async function deleteSession(event) {
+    const sessionName = event.target.parentNode.parentNode.querySelector(".sessionName").textContent
+    // supprimer la session de la base de données
+    await chrome.storage.local.remove(sessionName)
+    // actualiser l'affichage de la liste
+    printSessions()
+}
+
+
+function deleteList(event) {
+
+}
+
+
+
